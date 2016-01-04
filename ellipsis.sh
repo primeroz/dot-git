e3ee9a7bbb3db8376e361e2957d29faa0f0094e3
G. Roggemans <g.roggemans@grog.be>
2016-01-04 18:01:58 +0100

    Added ellipsis.sh script

---
+++ ellipsis.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+##############################################################################
+# @file ellipsis.sh
+# @date January, 2016
+# @author G. Roggemans <g.roggemans@grog.be>
+# @copyright Copyright (c) GROG [https://grog.be] 2016, All Rights Reserved
+# @license MIT
+##############################################################################
+
+##############################################################################
+
